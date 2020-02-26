cba67e6a17fa3b0a5c8864b73009fba9f31c5ea4
leowzhenkang <leowzk@hotmail.com>
2020-02-26 22:37:17 +0800

    added favourites and retweets counts

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 
 import tweets from 'tweets'
 
+class Favorite extends React.Component {
+  render() {
+    return(
+      <div>
+        {this.props.favorite_count}
+      </div>
+    );
+  }
+}
+
+class Retweet extends React.Component {
+  render() {
+    return(
+      <div>
+        {this.props.retweet_count}
+      </div>
+    );
+  }
+}
+
 class Created extends React.Component {
   render() {
     return(
@@ -29,8 +49,10 @@ class App extends React.Component {
   render() {
         let mapAllTweets = allTweet.map(tweet => {
             return <ul>
-                        <li><Text text = {tweet.text}/></li>
-                        <li><Created created_at = {tweet.created_at}/></li>
+                        <li>Tweet: <Text text = {tweet.text}/></li>
+                        <li>Date: <Created created_at = {tweet.created_at}/></li>
+                        <li>Retweets: <Retweet retweet_count = {tweet.retweet_count}/></li>
+                        <li>Favorites: <Favorite favorite_count = {tweet.favorite_count}/></li>
                    </ul>
         })
     return (
@@ -48,7 +70,7 @@ ReactDOM.render(<App />, element );//
 
 console.log("tweet react");
     console.log(tweets.tweets[1].user.screen_name)
-    console.log(tweets.tweets[1].created_at)
+    // console.log(tweets.tweets[1].created_at)
     // console.log(tweets.tweets[1].text)
-    console.log(tweets.tweets[1].retweet_count)
-    console.log(tweets.tweets[1].favorite_count)
+    // console.log(tweets.tweets[1].retweet_count)
+    // console.log(tweets.tweets[1].favorite_count)
